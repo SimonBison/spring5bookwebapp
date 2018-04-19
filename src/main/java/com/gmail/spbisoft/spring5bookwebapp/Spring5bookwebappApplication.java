73199eec04246bfc129516b720a020db1408ae55
sbi <szymon.bizon@hicron.com>
2018-04-19 09:38:43 +0200

    Initial commit

---
+++ src/main/java/com/gmail/spbisoft/spring5bookwebapp/Spring5bookwebappApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.spbisoft.spring5bookwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5bookwebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5bookwebappApplication.class, args);
+    }
+}
